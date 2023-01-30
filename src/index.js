11ddc29b0cbf18046f01ce56a7861ae87a0fa7d2
glen <glendymarmatute@gmail.com>
2023-01-30 08:56:18 -0500

    este es mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import validator from './validator.js';
-
 console.log(validator);
+//Estos son tres eventos, 1 para el imput y dos para el boton.
+document.getElementById("cardnumber").addEventListener("keyup", validator.habilitar);
+document.getElementById("btn").addEventListener("click", validator.validate);
+document.getElementById("btn").addEventListener("click", validator.maskify);
+
